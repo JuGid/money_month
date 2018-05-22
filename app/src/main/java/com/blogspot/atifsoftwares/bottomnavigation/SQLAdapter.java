29cb12e44d32b9c64bcef10ece0e2180d8a1dca2
Julien Gidel <gidjulien@gmail.com>
2018-05-22 23:56:34 +0200

    Initialisation du répertoire
    Ajout du projet android studio au répertoire

---
+++ app/src/main/java/com/blogspot/atifsoftwares/bottomnavigation/SQLAdapter.java
@@ -0,0 +1,18 @@
+package com.blogspot.atifsoftwares.bottomnavigation;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLAdapter extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
